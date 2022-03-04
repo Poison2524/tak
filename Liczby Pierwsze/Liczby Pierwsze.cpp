1c29a5283b5000f237ca26cae2995e3de4da3006
Poison2524 <ala.cieslak999@gmail.com>
2022-03-04 14:09:17 +0100

    Początek kodu

---
+++ Liczby Pierwsze/Liczby Pierwsze.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int lp = 0;
+    int p = 2l;
+    int n;
+
+    cout << "Podaj ilość liczb pierwszych: ";
+    cin >> n;
+    cout << endl;
+
+    for (int lp > n; lp++){
+
+    }
+}
