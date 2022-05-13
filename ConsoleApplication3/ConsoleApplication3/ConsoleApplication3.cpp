d49d023bdf019c1b010b188e109fe4e97d8ff473
Poison2524 <100200685+Poison2524@users.noreply.github.com>
2022-05-13 13:42:59 +0200

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int lp = 0;
+	int p = 2;
+	int x = 0;
+
+	cout << "Ile licz pierwszych chcesz wywwietlic?: ";
+	cin >> n;
+	cout << "Twoje iczby pierwsze to: ";
+	while (lp < n) {
+		x = 0;
+		for (int d = 2; d <= p - 1; d++) {
+			if (p % d == 0) {
+				x = 1;
+			}
+		}
+
+		if (x == 0) {
+			cout << p << " ";
+			lp = lp + 1;
+		}
+		p = p + 1;
+	}
+}
