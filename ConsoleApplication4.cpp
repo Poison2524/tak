0e5c7848abff73a497f5e34d53dd63d0c246da29
Poison2524 <100200685+Poison2524@users.noreply.github.com>
2022-05-23 11:44:26 +0200

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int bin(int table[], int szukana) {
+	int l = 0;
+	int p = 15;
+	int sr = (l + p) / 2;
+
+	while (l <= p) {
+		if (table[sr] == szukana) {
+			return sr;
+		}
+		else if (table[sr] > szukana) {
+			p = sr - 1;
+		}
+		else {
+			l = sr + 1;
+		}
+		sr = (l + p) / 2;
+	}
+	return -1;
+}
+
+int main(){
+	int szukana;
+	int table[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+
+	cout << "Podaj szukaną liczbę: ";
+	cin >> szukana;
+
+	if (bin(table, szukana) == -1) {
+		cout << "Nie znaleziono liczby.";
+	}
+	else {
+		cout << "Pozycja liczby " << szukana << " to: " << bin(table, szukana);
+	}
+}
